a3fcc0ce27cec75c813f522845efa0ee4e6e3aaa
František Milt <frantisek.milt@gmail.com>
2016-01-04 16:11:30 +0100

    Ensured compatibility with Lazarus/FPC

--- BitVector.pas
+++ BitVector.pas
@@ -132,7 +132,7 @@ const
 
 Function TBitVector.GetBit_LL(Index: Integer): Boolean;
 begin
-Result := (PByte(PtrUInt(fMemory) + PtrUInt(Index shr 3))^ shr (Index and 7)) and 1 <> 0;
+Result := ({%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(Index shr 3))^ shr (Index and 7)) and 1 <> 0;
 end;
 
 //------------------------------------------------------------------------------
@@ -141,12 +141,12 @@ Function TBitVector.SetBit_LL(Index: Integer; Value: Boolean): Boolean;
 var
   OldByte:  Byte;
 begin
-OldByte := PByte(PtrUInt(fMemory) + PtrUInt(Index shr 3))^;
+OldByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(Index shr 3))^;
 Result := (OldByte shr (Index and 7)) and 1 <> 0;
 If Value then
-  PByte(PtrUInt(fMemory) + PtrUInt(Index shr 3))^ := OldByte or (1 shl (Index and 7))
+  {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(Index shr 3))^ := OldByte or (1 shl (Index and 7))
 else
-  PByte(PtrUInt(fMemory) + PtrUInt(Index shr 3))^ := OldByte and not (1 shl (Index and 7));
+  {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(Index shr 3))^ := OldByte and not (1 shl (Index and 7));
 end;
 
 //------------------------------------------------------------------------------
@@ -234,7 +234,7 @@ If OwnsMemory then
                     For i := fCount to Min(Pred(Value),fCount or 7) do
                       SetBit_LL(i,False);
                   For i := Ceil(fCount / 8) to Pred(Value shr 3) do
-                    PByte(PtrUInt(fMemory) + PtrUInt(i))^ := 0;
+                    {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := 0;
                   If ((Value and 7) <> 0) and ((Value and not 7) >= fCount) then
                     For i := (Value and not 7) to Pred(Value) do
                       SetBit_LL(i,False);
@@ -278,11 +278,11 @@ If Idx2 > Idx1 then
         For i := Pred(Idx2 shr 3) downto Succ(Idx1 shr 3) do
           begin
             If Carry then
-              Temp := UInt16(PByte(PtrUInt(fMemory) + PtrUInt(i))^) or $0100
+              Temp := UInt16({%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^) or $0100
             else
-              Temp := UInt16(PByte(PtrUInt(fMemory) + PtrUInt(i))^) and $FEFF;
+              Temp := UInt16({%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^) and $FEFF;
             Carry := (Temp and 1) <> 0;
-            PByte(PtrUInt(fMemory) + PtrUInt(i))^ := Byte(Temp shr 1);
+            {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := Byte(Temp shr 1);
           end;
         // shift first byte and store carry
         For i := Idx1 to Pred(Idx1 or 7) do
@@ -316,11 +316,11 @@ If Idx2 > Idx1 then
         For i := Succ(Idx1 shr 3) to Pred(Idx2 shr 3) do
           begin
             If Carry then
-              Temp := (UInt16(PByte(PtrUInt(fMemory) + PtrUInt(i))^) shl 1) or 1
+              Temp := (UInt16({%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^) shl 1) or 1
             else
-              Temp := (UInt16(PByte(PtrUInt(fMemory) + PtrUInt(i))^) shl 1) and not 1;
+              Temp := (UInt16({%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^) shl 1) and not 1;
             Carry := (Temp and $100) <> 0;
-            PByte(PtrUInt(fMemory) + PtrUInt(i))^ := Byte(Temp);
+            {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := Byte(Temp);
           end;
         // shift last byte and store carry
         For i := Idx2 downto Succ(Idx2 and not 7) do
@@ -621,7 +621,7 @@ If CheckIndex(FromIdx,True) and CheckIndex(ToIdx,True) then
       For i := FromIdx to Min(ToIdx,FromIdx or 7) do
         SetBit_LL(i,Value);
     For i := Ceil(FromIdx / 8) to Pred(Succ(ToIdx) shr 3) do
-      PByte(PtrUInt(fMemory) + PtrUInt(i))^ := Ord(Value) * $FF;
+      {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := Ord(Value) * $FF;
     If ((ToIdx and 7) < 7) and ((ToIdx and not 7) > FromIdx) then
       For i := (ToIdx and not 7) to ToIdx do
         SetBit_LL(i,Value);
@@ -639,7 +639,7 @@ begin
 If fCount > 0 then
   begin
     For i := 0 to Pred(fCount shr 3) do
-      PByte(PtrUInt(fMemory) + PtrUInt(i))^ := $FF * Ord(Value);
+      {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := $FF * Ord(Value);
     For i := (fCount and not 7) to Pred(fCount) do
       SetBit_LL(i,Value);
     fPopCount := fCount * Ord(Value);      
@@ -665,7 +665,7 @@ If CheckIndex(FromIdx,True) and CheckIndex(ToIdx,True) then
       For i := FromIdx to Min(ToIdx,FromIdx or 7) do
         SetBit_LL(i,not GetBit_LL(i));
     For i := Ceil(FromIdx / 8) to Pred(Succ(ToIdx) shr 3) do
-      PByte(PtrUInt(fMemory) + PtrUInt(i))^ := not PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+      {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := not {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
     If ((ToIdx and 7) < 7) and ((ToIdx and not 7) > FromIdx) then
       For i := (ToIdx and not 7) to ToIdx do
         SetBit_LL(i,not GetBit_LL(i));
@@ -683,7 +683,7 @@ begin
 If fCount > 0 then
   begin
     For i := 0 to Pred(fCount shr 3) do
-      PByte(PtrUInt(fMemory) + PtrUInt(i))^ := not PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+      {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := not {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
     For i := (fCount and not 7) to Pred(fCount) do
       SetBit_LL(i,not GetBit_LL(i));
     fPopCount := fCount - fPopCount;
@@ -737,7 +737,7 @@ If fCount > 0 then
   begin
     For i := 0 to Pred(fCount shr 3) do
       begin
-        WorkByte := PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+        WorkByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
         If WorkByte <> 0 then
           begin
             Result := (i * 8) + ScanByte(WorkByte);
@@ -770,7 +770,7 @@ If fCount > 0 then
   begin
     For i := 0 to Pred(fCount shr 3) do
       begin
-        WorkByte := PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+        WorkByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
         If WorkByte <> $FF then
           begin
             Result := (i * 8) + ScanByte(WorkByte);
@@ -805,7 +805,7 @@ If fCount > 0 then
       If GetBit_LL(Result) then Exit;
     For i := Pred(fCount shr 3) downto 0 do
       begin
-        WorkByte := PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+        WorkByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
         If WorkByte <> 0 then
           begin
             Result := (i * 8) + ScanByte(WorkByte);
@@ -838,7 +838,7 @@ If fCount > 0 then
       If not GetBit_LL(Result) then Exit;
     For i := Pred(fCount shr 3) downto 0 do
       begin
-        WorkByte := PByte(PtrUInt(fMemory) + PtrUInt(i))^;
+        WorkByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^;
         If WorkByte <> $FF then
           begin
             Result := (i * 8) + ScanByte(WorkByte);
@@ -863,10 +863,10 @@ If fOwnsMemory then
     If (fCount and 7) = 0 then
       begin
         Capacity := fCount + Count;
-        System.Move(Memory^,Pointer(PtrUInt(fMemory) + PtrUInt(fCount shr 3))^,Count shr 3);
+        System.Move(Memory^,{%H-}Pointer({%H-}PtrUInt(fMemory) + PtrUInt(fCount shr 3))^,Count shr 3);
         If (Count and 7) <> 0 then
           begin
-            WorkByte := PByte(PtrUInt(Memory) + PtrUInt(Count shr 3))^;
+            WorkByte := {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(Count shr 3))^;
             For i := 0 to Pred(Count - (Count and not 7)) do
               SetBit_LL(fCount + (Count and not 7) + i,(WorkByte shr i) and 1 <> 0);
           end;    
@@ -924,7 +924,7 @@ If fOwnsMemory then
       System.Move(Memory^,fMemory^,Count shr 3);
       If (Count and 7) <> 0 then
         begin
-          WorkByte := PByte(PtrUInt(Memory) + PtrUInt(Count shr 3))^;
+          WorkByte := {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(Count shr 3))^;
           For i := (Count and not 7) to Pred(Count) do
             SetBit_LL(i,(WorkByte shr (i and 7)) and 1 <> 0);
         end;
@@ -958,11 +958,11 @@ If fOwnsMemory then
     try
       If Count > fCount then Self.Count := Count;
       For i := 0 to Pred(Count shr 3) do
-        PByte(PtrUInt(fMemory) + PtrUInt(i))^ := PByte(PtrUInt(fMemory) + PtrUInt(i))^ or
-                                                 PByte(PtrUInt(Memory) + PtrUInt(i))^;
+        {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ or
+                                                           {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(i))^;
       If (Count and 7) <> 0 then
         begin
-          WorkByte := PByte(PtrUInt(Memory) + PtrUInt(Count shr 3))^;
+          WorkByte := {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(Count shr 3))^;
           For i := (Count and not 7) to Pred(Count) do
             SetBit_LL(i,GetBit_LL(i) or ((WorkByte shr (i and 7)) and 1 <> 0));
         end;
@@ -1000,11 +1000,11 @@ If fOwnsMemory then
           Fill(i,Pred(fCount),True);
         end;
       For i := 0 to Pred(Count shr 3) do
-        PByte(PtrUInt(fMemory) + PtrUInt(i))^ := PByte(PtrUInt(fMemory) + PtrUInt(i))^ and 
-                                                 PByte(PtrUInt(Memory) + PtrUInt(i))^;
+        {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ and 
+                                                           {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(i))^;
       If (Count and 7) <> 0 then
         begin
-          WorkByte := PByte(PtrUInt(Memory) + PtrUInt(Count shr 3))^;
+          WorkByte := {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(Count shr 3))^;
           For i := (Count and not 7) to Pred(Count) do
             SetBit_LL(i,GetBit_LL(i) and ((WorkByte shr (i and 7)) and 1 <> 0));
         end;
@@ -1037,11 +1037,11 @@ If fOwnsMemory then
     try
       If Count > fCount then Self.Count := Count;
       For i := 0 to Pred(Count shr 3) do
-        PByte(PtrUInt(fMemory) + PtrUInt(i))^ := PByte(PtrUInt(fMemory) + PtrUInt(i))^ xor
-                                                 PByte(PtrUInt(Memory) + PtrUInt(i))^;
+        {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ xor
+                                                           {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(i))^;
       If (Count and 7) <> 0 then
         begin
-          WorkByte := PByte(PtrUInt(Memory) + PtrUInt(Count shr 3))^;
+          WorkByte := {%H-}PByte({%H-}PtrUInt(Memory) + PtrUInt(Count shr 3))^;
           For i := (Count and not 7) to Pred(Count) do
             SetBit_LL(i,GetBit_LL(i) xor ((WorkByte shr (i and 7)) and 1 <> 0));
         end;
@@ -1071,7 +1071,7 @@ Result := False;
 If (fCount = Vector.Count) and (fPopCount = Vector.PopCount) then
   begin
     For i := 0 to Pred(fCount shr 3) do
-      If PByte(PtrUInt(fMemory) + PtrUInt(i))^ <> PByte(PtrUInt(Vector.Memory) + PtrUInt(i))^ then Exit;
+      If {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(i))^ <> {%H-}PByte({%H-}PtrUInt(Vector.Memory) + PtrUInt(i))^ then Exit;
     If (fCount and 7) <> 0 then
       For i := (fCount and not 7) to Pred(fCount) do
         If GetBit_LL(i) <> Vector[i] then Exit;
@@ -1088,7 +1088,7 @@ begin
 Stream.WriteBuffer(fMemory^,fCount shr 3);
 If (fCount and 7) <> 0 then
   begin
-    TempByte := PByte(PtrUInt(fMemory) + PtrUInt(fCount shr 3))^ and (Byte($FF) shr (8 - (fCount and 7)));
+    TempByte := {%H-}PByte({%H-}PtrUInt(fMemory) + PtrUInt(fCount shr 3))^ and (Byte($FF) shr (8 - (fCount and 7)));
     Stream.WriteBuffer(TempByte,1);
   end;
 end;
